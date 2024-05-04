f17488c2a21dd2c59d2e41b6d39ccacc314b3792
guguskatistareidelas <144486173+guguskatistareidelas@users.noreply.github.com>
2024-05-04 19:04:36 -0300

    Update login.js
    eu atualizei o login.js para a nova versão da pagina de login

--- static/js/login.js
+++ static/js/login.js
@@ -1,35 +1,89 @@
-const bloco_login = $("#bloco_login")
-const inpLoginUsuario = $("#inp_usuario_login")
-const placeholderLoginUsuario = $("#placeholder_usuario_login")
-const inpLoginSenha = $("#inp_senha_login")
-const placeholderLoginSenha = $("#placeholder_senha_login")
-const btn_login = $("#botao_login")
-const btnTrocaSigninSignup = $("#trocar_signin_signup")
-const bloco_login_inputs = $("#bloco_login_inputs")
-//variaveis login
+// pegando os elementos
+const body = document.getElementById('body');
+const form = document.getElementById('form');
+const inputColunm = document.getElementById('input-colunm');
 
-inpLoginUsuario.on("focus", function () {
-    placeholderLoginUsuario.css("opacity", "0")
-})
+const detail1 = document.getElementById('detail1');
+const detail2 = document.getElementById('detail2');
+const detail3 = document.getElementById('detail3');
+const detail4 = document.getElementById('detail4');
 
-inpLoginSenha.on("focus", function () {
-    placeholderLoginSenha.css("opacity", "0")
-})
+const matriculaInput = document.getElementById('matrícula');
+const passwordInput = document.getElementById('password');
+const eye = document.getElementById('eye-icon');
 
-btnTrocaSigninSignup.on("click", function() {
-    let novoAttr = ""
-    if (btn_login.attr("name") == "signin") {
-        novoAttr = "signup"
-        bloco_login_inputs.append('<div class="bloco_filho_bloco_login" id="bloco_login_nome"><div class="container">●</div><span class="placeholder_login" id="placeholder_nome_login">nome</span><input type="text" id="inp_nome_login" class="inp_login" name="nome" required></div>')
-        btn_login.css("margin-top", "calc(10vh - 50px)")
+const button = document.getElementById('button');
+const goToRegister = document.getElementById('goToRegister');
+
+// função de loop
+function loop() {
+    // pegando as larguras e alturas  
+    var formWidth = form.offsetWidth
+    var formHeight = form.offsetHeight 
+    var bodyWidth = body.offsetWidth
+    var passwordInputWidth = passwordInput.offsetWidth
+    var inputColunmHeight = inputColunm.offsetHeight
+
+    // calculando os valores do posicionamento animado dos elementos
+    var iconTopValue = 245.5 + ((formHeight - inputColunmHeight) / 2)
+    var iconRigthValue = (bodyWidth - passwordInputWidth) / 2 + 15
+    var detail1RightValue = ((bodyWidth - formWidth) / 2) - 16
+    var detail2TopValue = 100 + formHeight
+    var detail2LeftValue = (bodyWidth - formWidth) / 2 - 27
+    var detail3rightValue = ((bodyWidth - formWidth) / 2) - 32.5
+    var detail4LeftValue = ((bodyWidth - formWidth) / 2) - 32.5
+
+    //setando a posição dos elementos
+    detail1.style.right = detail1RightValue + 'px';
+    detail2.style.top = detail2TopValue + 'px';
+    detail2.style.left = detail2LeftValue + 'px';
+    detail3.style.right =  detail3rightValue+ 'px';
+    detail4.style.left = detail4LeftValue + 'px';
+    eye.style.right = iconRigthValue + 'px';
+    eye.style.top = iconTopValue + 'px';
+
+    setTimeout(loop, 0);
+  }
+  
+// Inicia o loop
+loop();
+
+//eventListeners:
+  //o botão do olho foi clicado?
+  eye.addEventListener('click', function() {
+    if (eye.className == 'fas fa-eye') {
+      passwordInput.type = 'password';
+      eye.className = 'fas fa-eye-slash';
     }
-    else {
-        novoAttr = "signin"
-        $("#bloco_login_nome").remove()
-        btn_login.css("margin-top", "15vh")
+    else{
+      passwordInput.type = "text"
+      eye.className = 'fas fa-eye';
     }
-
-    btn_login.attr("name", novoAttr)
-})
-
-//funções login
+  });
+  //animação do botão do olho
+  passwordInput.addEventListener('mousedown', function() {
+    eye.style.transform = "translateY(6px) translateX(-6px)";
+  })
+  passwordInput.addEventListener('mouseup', function() {
+    eye.style.transform = "translateY(0px) translateX(0px)";
+  })
+  // botão de registrar foi clicado?
+  button.addEventListener('click', function() {
+    if ( matriculaInput.value == "" || passwordInput.value == "") {
+      matriculaInput.style.borderColor = "#0F3B5E";
+      passwordInput.style.borderColor = "#0F3B5E";
+      if (matriculaInput.value == "") {
+        matriculaInput.style.borderColor = "#ff0000";
+      }
+      if (passwordInput.value == "") {
+        passwordInput.style.borderColor = "#ff0000";
+      }
+    }else{
+      matriculaInput.style.borderColor = "#0F3B5E";
+      passwordInput.style.borderColor = "#0F3B5E";
+    }
+  })
+  // botão de registrar-se foi clicado?
+  goToRegister.addEventListener("click",function (){
+    window.location.href = '/'
+  })
