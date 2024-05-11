92ba259d349278c5e40f2ed1d77c3b967e11958c
guguskatistareidelas <144486173+guguskatistareidelas@users.noreply.github.com>
2024-05-11 20:36:30 -0300

    Update configuracoes.js

--- static/js/configuracoes.js
+++ static/js/configuracoes.js
@@ -1,4 +1,19 @@
-const btnSubmit = $("#btn_submit")
-const inpFotoDePerfil = $("#inp_foto_perfil")
+const button = document.getElementById("btn_submit");
+const blurr = document.getElementById("blur");
+const inputs = document.getElementsByTagName("input");
+const popUp = document.getElementById("popUp");
+const form = document.getElementById("form");
 
-
+button.addEventListener("click", () => {
+    blurr.style.filter = "blur(10px)";
+    popUp.style.transform = "translate(-50%, -50%) scale(1)"
+    popUp.style.visibility = "visible"
+})
+form.getElementsByTagName("i")[0].addEventListener("click", () => {
+    window.location.href = "/"
+})
+popUp.getElementsByTagName("i")[0].addEventListener("click",()=>{
+    blurr.style.filter = "blur(0px)";
+    popUp.style.transform = "translate(-50%, 0) scale(0)"
+    popUp.style.visibility = "hidden"
+})
